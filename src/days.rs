6ebc8e1c2c36b88c3cd2c5879fbcb46942807046
Jonathan Keelan <jonathan.keelan@faculty.ai>
2021-12-23 19:40:29 +0000

    day 9 is not done yet

--- src/days.rs
+++ src/days.rs
@@ -274,3 +274,105 @@ fn day_6() {
 }
 
 // I'm naughty and did day 7 in an ipython terminal. Part 1 was the median, part 2 was the mean.
+
+fn share_char(a: &str, b: &str) -> String {
+    // get which one is shorter
+    let (shorter, longer) = if a.len() > b.len() { (b, a) } else { (a, b) };
+
+    // fill the set with the characters from the shorter string
+    let set: HashSet<char> = shorter.chars().collect();
+
+    let v = longer
+        .chars()
+        .filter(|c| set.contains(&c))
+        .collect::<String>();
+    return v;
+}
+
+fn matched(a: &str, b: &str) -> bool {
+    let overlap = share_char(a, b);
+    if overlap.len() == a.len() && a.len() == b.len() {
+        return true;
+    }
+    return false;
+}
+
+pub fn day_8() {
+    let mut segments = HashMap::new();
+    segments.insert(0, "abcefg");
+    segments.insert(1, "cf");
+    segments.insert(2, "acdeg");
+    segments.insert(3, "acdfg");
+    segments.insert(4, "bcdf");
+    segments.insert(5, "abdfg");
+    segments.insert(6, "abdefg");
+    segments.insert(7, "acf");
+    segments.insert(8, "abcdefg");
+    segments.insert(9, "abcdfg");
+
+    let file_contents = fs::read_to_string("data/day8.txt").unwrap();
+    let contents: Vec<(Vec<&str>, Vec<&str>)> = file_contents
+        .split("\n")
+        .into_iter()
+        .map(|x| {
+            let mut split = x.split(" | ");
+            let lhs = split.next().unwrap().split(" ").collect::<Vec<&str>>();
+            let rhs = split.next().unwrap().split(" ").collect::<Vec<&str>>();
+            (lhs, rhs)
+        })
+        .collect();
+    // Calculate baseline signatures
+    let mut signatures: HashMap<i32, HashSet<i32>> = HashMap::new();
+    for i in [0, 2, 3, 5, 6, 9] {
+        let mut temp = HashSet::new();
+        for j in [1, 4, 7, 8] {
+            temp.insert(
+                share_char(segments.get(&i).unwrap(), segments.get(&j).unwrap()).len() as i32,
+            );
+        }
+        signatures.insert(i, temp);
+    }
+    let mut total_sum = 0;
+    for (line, res) in contents {
+        let lens: Vec<i32> = line.iter().map(|v| v.len() as i32).collect();
+        let mut line_map = HashMap::new();
+        for i in 0..lens.len() {
+            match lens[i] {
+                2 => line_map.insert(1, line[i]),
+                3 => line_map.insert(7, line[i]),
+                4 => line_map.insert(4, line[i]),
+                7 => line_map.insert(8, line[i]),
+                _ => Some("lol"),
+            };
+        }
+        // calculate signatures for the remaining numbers
+        for i in 0..line.len() {
+            let mut temp = HashSet::new();
+            // iterate over 1, 4, 7, 8
+            for k in [1, 4, 7, 8] {
+                // Calculate signature as the number of overlapping characters
+                let other = line_map.get(&k).unwrap();
+                temp.insert(share_char(other, line[i]).len() as i32);
+            }
+            for (j, main_sig) in &signatures {
+                if &temp == main_sig {
+                    line_map.insert(*j, line[i]);
+                }
+            }
+        }
+
+        // convert res and sum
+        let mut sum = 0;
+        let mut mult = 1000;
+        for v in res {
+            for (num, s) in &line_map {
+                if matched(v, s) {
+                    sum += mult * num;
+                    mult = mult / 10;
+                }
+            }
+        }
+        total_sum += sum;
+    }
+    println!("{}", total_sum);
+}
--- src/main.rs
+++ src/main.rs
@@ -7,104 +7,125 @@ use std::io::BufRead;
 use std::io::BufReader;
 // mod days;
 
-fn share_char(a: &str, b: &str) -> String {
-    // get which one is shorter
-    let (shorter, longer) = if a.len() > b.len() { (b, a) } else { (a, b) };
-
-    // fill the set with the characters from the shorter string
-    let set: HashSet<char> = shorter.chars().collect();
-
-    let v = longer
-        .chars()
-        .filter(|c| set.contains(&c))
-        .collect::<String>();
-    return v;
+fn get_risk(vals: &Vec<Vec<i32>>) -> i32 {
+    let dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+    let mut sum_risk = 0;
+    for (i, row) in vals.iter().enumerate() {
+        for (j, v) in row.iter().enumerate() {
+            let mut is_min = true;
+            for (dx, dy) in dirs {
+                let x = i as i32 + dx;
+                let y = j as i32 + dy;
+                if x < 0 || y < 0 || x >= vals.len() as i32 || y >= vals[0].len() as i32 {
+                    continue;
+                } else {
+                    if v >= &vals[x as usize][y as usize] {
+                        is_min = false
+                    }
+                }
+            }
+            if is_min {
+                sum_risk += v + 1;
+            }
+        }
+    }
+    return sum_risk;
 }
 
-fn matched(a: &str, b: &str) -> bool {
-    let overlap = share_char(a, b);
-    if overlap.len() == a.len() && a.len() == b.len() {
-        return true;
+fn is_basin(i: usize, j: usize, vals: &Vec<Vec<i32>>) -> bool {
+    let v = vals[i][j];
+    let dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+    let mut is_min = true;
+    for (dx, dy) in dirs {
+        let x = i as i32 + dx;
+        let y = j as i32 + dy;
+        if x < 0 || y < 0 || x >= vals.len() as i32 || y >= vals[0].len() as i32 {
+            continue;
+        } else {
+            if v >= vals[x as usize][y as usize] {
+                is_min = false
+            }
+        }
     }
-    return false;
+    return is_min;
+}
+fn find_basins(vals: &Vec<Vec<i32>>) -> &Vec<(usize, usize)> {
+    let mut basins = Vec::new();
+    for (i, row) in vals.iter().enumerate() {
+        for (j, v) in row.iter().enumerate() {
+            if is_basin(i, j, vals) {
+                basins.push((i, j));
+            }
+        }
+    }
+    return &basins;
+}
+
+fn find_basin<'a>(
+    x: &usize,
+    y: &usize,
+    vals: &'a Vec<Vec<i32>>,
+    basins: &'a mut HashMap<&'a (usize, usize), i32>,
+) -> &'a (usize, usize) {
+    match is_basin(*x, *y, &vals) {
+        true => return &(*x, *y),
+        false => {
+            let dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)];
+            let v = vals[*x][*y];
+            for (dx, dy) in dirs {
+                let k = *x as i32 + dx;
+                let l = *y as i32 + dy;
+                if k < 0 || l < 0 || k >= vals.len() as i32 || l >= vals[0].len() as i32 {
+                    continue;
+                } else {
+                    if v < vals[k as usize][l as usize] {
+                        let basin = find_basin(&(k as usize), &(l as usize), vals, basins);
+                        let count = *basins.get(&basin).unwrap_or(&0);
+                        basins.insert(basin, count + 1);
+                        return basin;
+                    }
+                }
+            }
+            return &(0, 0);
+        }
+    }
 }
 
 fn main() {
-    let mut segments = HashMap::new();
-    segments.insert(0, "abcefg");
-    segments.insert(1, "cf");
-    segments.insert(2, "acdeg");
-    segments.insert(3, "acdfg");
-    segments.insert(4, "bcdf");
-    segments.insert(5, "abdfg");
-    segments.insert(6, "abdefg");
-    segments.insert(7, "acf");
-    segments.insert(8, "abcdefg");
-    segments.insert(9, "abcdfg");
-
-    let file_contents = fs::read_to_string("data/day8.txt").unwrap();
-    let contents: Vec<(Vec<&str>, Vec<&str>)> = file_contents
+    let vals = fs::read_to_string("./data/day9.txt")
+        .unwrap()
         .split("\n")
-        .into_iter()
         .map(|x| {
-            let mut split = x.split(" | ");
-            let lhs = split.next().unwrap().split(" ").collect::<Vec<&str>>();
-            let rhs = split.next().unwrap().split(" ").collect::<Vec<&str>>();
-            (lhs, rhs)
+            x.split("")
+                .filter_map(|v| v.parse::<i32>().ok())
+                .collect::<Vec<i32>>()
         })
-        .collect();
-    // Calculate baseline signatures
-    let mut signatures: HashMap<i32, HashSet<i32>> = HashMap::new();
-    for i in [0, 2, 3, 5, 6, 9] {
-        let mut temp = HashSet::new();
-        for j in [1, 4, 7, 8] {
-            temp.insert(
-                share_char(segments.get(&i).unwrap(), segments.get(&j).unwrap()).len() as i32,
-            );
-        }
-        signatures.insert(i, temp);
-    }
-    let mut total_sum = 0;
-    for (line, res) in contents {
-        let lens: Vec<i32> = line.iter().map(|v| v.len() as i32).collect();
-        let mut line_map = HashMap::new();
-        for i in 0..lens.len() {
-            match lens[i] {
-                2 => line_map.insert(1, line[i]),
-                3 => line_map.insert(7, line[i]),
-                4 => line_map.insert(4, line[i]),
-                7 => line_map.insert(8, line[i]),
-                _ => Some("lol"),
-            };
-        }
-        // calculate signatures for the remaining numbers
-        for i in 0..line.len() {
-            let mut temp = HashSet::new();
-            // iterate over 1, 4, 7, 8
-            for k in [1, 4, 7, 8] {
-                // Calculate signature as the number of overlapping characters
-                let other = line_map.get(&k).unwrap();
-                temp.insert(share_char(other, line[i]).len() as i32);
-            }
-            for (j, main_sig) in &signatures {
-                if &temp == main_sig {
-                    line_map.insert(*j, line[i]);
-                }
-            }
-        }
+        .collect::<Vec<Vec<i32>>>();
 
-        // convert res and sum
-        let mut sum = 0;
-        let mut mult = 1000;
-        for v in res {
-            for (num, s) in &line_map {
-                if matched(v, s) {
-                    sum += mult * num;
-                    mult = mult / 10;
+    println!("Risk is: {}", get_risk(&vals));
+
+    let basins = find_basins(&vals);
+    let mut basin_counts: HashMap<&(usize, usize), i32> = HashMap::new();
+    let mut basin_cache: HashMap<&(usize, usize), (usize, usize)> = HashMap::new();
+    for key in basins {
+        basin_counts.insert(key, 0);
+    }
+
+    for (i, row) in vals.iter().enumerate() {
+        for (j, v) in row.iter().enumerate() {
+            if *v == 9 || basin_counts.contains_key(&(i, j)) {
+                continue;
+            } else if basin_cache.contains_key(&(i, j)) {
+                let basin = basin_cache.get(&(i, j)).unwrap();
+                let count = basin_counts.get(basin).unwrap();
+                basin_counts.insert(basin, count + 1);
+            } else {
+                let mut new_basin_counts = HashMap::new();
+                let (basin, count) = find_basin(&i, &j, &vals, &mut new_basin_counts);
+                for (key, value) in new_basin_counts {
+                    let count = *basin_counts.get(key).unwrap();
                 }
             }
         }
-        total_sum += sum;
     }
-    println!("{}", total_sum);
 }
