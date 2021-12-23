7a5fe73a69b3e0f15130c4f43395bed20c2e26aa
Jonathan Keelan <jonathan.keelan@faculty.ai>
2021-12-23 21:27:53 +0000

    day 11 done

--- src/main.rs
+++ src/main.rs
@@ -6,72 +6,68 @@ use std::io;
 use std::io::BufRead;
 use std::io::BufReader;
 
-fn is_close_bracket(c: &char) -> bool {
-    match c {
-        '>' => return true,
-        '}' => return true,
-        ']' => return true,
-        ')' => return true,
-        _ => return false,
+fn flash(grid: &mut Vec<Vec<i32>>, flashers: &mut Vec<(usize, usize)>, i: i32, j: i32) -> i32 {
+    let mut count = 0;
+    if grid[i as usize][j as usize] <= 9 || flashers.contains(&(i as usize, j as usize)) {
+        return count;
     }
-}
-
-fn is_open_bracket(c: &char) -> bool {
-    match c {
-        '<' => return true,
-        '{' => return true,
-        '[' => return true,
-        '(' => return true,
-        _ => return false,
+    count += 1;
+    flashers.push((i as usize, j as usize));
+    for r in -1..=1 {
+        for c in -1..=1 {
+            if r == 0 && c == 0 {
+                continue;
+            }
+            if i + r < 0 || i + r >= grid.len() as i32 {
+                continue;
+            }
+            if j + c < 0 || j + c >= grid[0].len() as i32 {
+                continue;
+            }
+            let x = (i + r) as usize;
+            let y = (j + c) as usize;
+            grid[x][y] += 1;
+            if !flashers.contains(&(x, y)) && grid[x][y] > 9 {
+                count += flash(grid, flashers, x as i32, y as i32);
+            }
+        }
     }
+    return count;
 }
 // mod days;
 fn main() {
-    let scores = HashMap::from([('}', 3), (')', 1), (']', 2), ('>', 4)]);
-    let matcher = HashMap::from([('{', '}'), ('[', ']'), ('<', '>'), ('(', ')')]);
-    let mut score = 0;
-    let mut line_scores: Vec<i64> = Vec::new();
-    let file_contents = std::fs::read_to_string("data/day10.txt").unwrap();
-    let lines = file_contents.split("\n").collect::<Vec<&str>>();
-    for line in lines {
-        let mut buffer: Vec<char> = Vec::new();
-        let mut good_line = true;
-        for c in line.chars() {
-            if is_open_bracket(&c) {
-                buffer.push(c);
-            } else if buffer.len() > 0 && is_close_bracket(&c) {
-                let old_bracket = buffer.pop().unwrap();
-                match (old_bracket, c) {
-                    ('{', '}') => {}
-                    ('[', ']') => {}
-                    ('<', '>') => {}
-                    ('(', ')') => {}
-                    (_, new) => {
-                        good_line = false;
-                        break;
-                    }
+    let lines = std::fs::read_to_string("data/day11.txt").unwrap();
+    let mut grid: Vec<Vec<i32>> = lines
+        .split("\n")
+        .map(|v| {
+            v.chars()
+                .flat_map(|n| n.to_string().parse::<i32>())
+                .collect()
+        })
+        .collect();
+    let mut total_flashes = 0;
+    for step in 0..1000 {
+        let mut flashers: Vec<(usize, usize)> = Vec::new();
+        for i in 0..grid.len() {
+            for j in 0..grid[0].len() {
+                grid[i][j] += 1;
+                if grid[i][j] > 9 {
+                    flashers.push((i, j));
                 }
-            } else {
-                println!("{}", c);
-                good_line = false;
-                break;
             }
         }
-        let mut line_sum: i64 = 0;
-        if good_line {
-            let mut auto_complete = buffer
-                .iter()
-                .map(|v| scores.get(matcher.get(&v).unwrap()).unwrap())
-                .collect::<Vec<&i32>>();
-            auto_complete.reverse();
-            for value in auto_complete {
-                line_sum *= 5;
-                line_sum += *value as i64;
-            }
-            line_scores.push(line_sum);
+        let mut flash_record: Vec<(usize, usize)> = Vec::new();
+        let mut flash_count = 0;
+        for (i, j) in flashers {
+            flash_count += flash(&mut grid, &mut flash_record, i as i32, j as i32);
+        }
+        if flash_count == (grid.len() * grid[0].len()) as i32 {
+            println!("ALL FLASH @ {}", step + 1)
+        }
+        total_flashes += flash_count;
+        for (i, j) in flash_record {
+            grid[i][j] = 0;
         }
     }
-    line_scores.sort();
-
-    println!("Sum is: {}", line_scores[line_scores.len() / 2 as usize]);
+    println!("total flashes: {}", total_flashes)
 }
